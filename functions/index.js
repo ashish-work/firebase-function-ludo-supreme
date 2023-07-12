4c2a88376e17445868ef8c0897f65960969571a5
Ashish Gupta <ashishgupta@Ashishs-MacBook-Pro.local>
2023-07-12 19:42:47 +0530

    create game doc in firebase

--- functions/index.js
+++ functions/index.js
@@ -8,15 +8,69 @@
  */
 "use strict";
 const {logger} = require("firebase-functions");
-// const {onRequest} = require("firebase-functions/v2/https");
 const functions = require("firebase-functions/v1");
 const admin = require("firebase-admin");
 
 admin.initializeApp();
 
+const constructGame = (players) => {
+  const details = {};
+  details["players"] = players;
+  for (let i=0; i<players.length; i++) {
+    const playerId = players[i];
+    details[playerId] = {
+      move: 0,
+    };
+  }
+
+  return JSON.stringify(details);
+};
+
 exports.createWaitingRoom = functions.database.ref("loggedInUsers").onWrite(
     async (change) => {
+      logger.log("starting createWaitingRoom");
+      let docRef = null;
       const db = admin.database();
-      const docRef = db.ref("waitingRoom").push({"test": "test"});
-      logger.log("created docRef", docRef);
+      db.ref("loggedInUsers")
+          .on("value", (snapshot) => {
+            logger.log("extracting players");
+            if (snapshot.numChildren() < 3) {
+              const value = snapshot.val();
+              const players = [];
+              for (const key in value) {
+                if (Object.hasOwn(value, key)) {
+                  players.push(key);
+                }
+              }
+              docRef = db.ref("waitingRoom")
+                  .push({
+                    "created": admin.database.ServerValue.TIMESTAMP,
+                    "duration": 120,
+                    "players": players,
+                  }).key;
+              logger.log("created docRef", docRef);
+              db.ref("waitingRoom")
+                  .child(docRef)
+                  .child("players")
+                  .get().then((snapshot) => {
+                    const players = snapshot.val();
+                    logger.log("players", players);
+                    logger.log("constructGame(players)",
+                        constructGame(players));
+                    const gameKey = db.ref("games")
+                        .push({"players": players}).key;
+                    db.ref("games").child("currTurn").set(players[0]);
+                    for (let i=0; i<players.length; i++) {
+                      db.ref("games").child(gameKey).child(players[i]).set({
+                        "move": 0,
+                      });
+                      db.ref("users").child(players[i]).set({
+                        "game": gameKey,
+                      });
+                    }
+                  }).catch((error)=>{
+                    logger.log(error);
+                  });
+            }
+          });
     });
